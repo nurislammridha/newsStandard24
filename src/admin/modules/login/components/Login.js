f0c157ef5da81f3565b13ca7170898b54ec254bc
nurislam <mridha851@gmail.com>
2021-07-09 13:30:02 +0600

    buy now => pay now with logout

--- src/admin/modules/login/components/Login.js
+++ src/admin/modules/login/components/Login.js
@@ -1,9 +1,60 @@
 import React from "react";
-
+import { Card, Button } from "react-bootstrap";
+import logo from "../../../assets/images/headerIcon.png";
+import Select from "react-select";
 const Login = () => {
   return (
     <>
-      <h1>Admin Login Page</h1>
+      <div className="login-body">
+        <div>
+          <Card style={{ width: "44rem" }}>
+            <Card.Body>
+              <Card.Text>
+                <div className="row">
+                  <div className="col-sm-6">
+                    <h2 className="text-center text-muted">Login</h2>
+                    <select className="form-control mb-2">
+                      <option>Select Your Role</option>
+                      <option>Admin</option>
+                      <option>Writter</option>
+                    </select>
+                    <div className="input-group mb-2">
+                      <div className="input-group-prepend">
+                        <div className="input-group-text">@</div>
+                      </div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="inlineFormInputGroup"
+                        placeholder="Username"
+                      />
+                    </div>
+                    <div className="input-group mb-2">
+                      <div className="input-group-prepend">
+                        <div className="input-group-text">@</div>
+                      </div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="inlineFormInputGroup"
+                        placeholder="Username"
+                      />
+                    </div>
+                    <div>
+                      <a className="btn btn-secondary float-right">Submit</a>
+                    </div>
+                  </div>
+                  <div className="col-sm-6">
+                    <div className="login-body pl-3">
+                      <img width="300" src={logo} />
+                    </div>
+                  </div>
+                </div>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
     </>
   );
 };
